2d0a4b1c4b1a17adbbc89ccbd5dfd0f65422d043
Angie Drucks <angie.drucks@gmail.com>
2024-11-21 23:30:02 +0100

    Add dynamic file naming to prevent overwriting output images and ensure unique filenames.

--- python_creative_filter/SurrealisticImageFilter/apply_surreal_filter.py
+++ python_creative_filter/SurrealisticImageFilter/apply_surreal_filter.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageEnhance, ImageFilter
 import matplotlib.pyplot as plt
+import os
 
 # load picture
 image_path = "honeyglas.jpeg"
@@ -26,6 +27,29 @@ axes[1].axis("off")
 plt.tight_layout()
 plt.show()
 
+# dynamic folder relativ addressed to the script
+script_dir = os.path.dirname(os.path.abspath(__file__))  # Verzeichnis des aktuellen Skripts
+output_folder = os.path.join(script_dir, "output")  # Ordner "output" für den Filter
+
+# be safe that folder exists
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+
+# function for incrementing
+def get_unique_filename(folder, base_name, extension):
+    i = 1
+    while True:
+        file_name = f"{base_name}_{i}.{extension}"
+        file_path = os.path.join(folder, file_name)
+        if not os.path.exists(file_path):
+            return file_path
+        i += 1
+
+
+# generate data path wit increment
+output_path = get_unique_filename(output_folder, "Surrealistic_Filtered_Image", "jpg")
+
 # save it
-final_image.save("Surrealistic_Filtered_Image.jpg")
-print("Surrealistic image saved as 'Surrealistic_Filtered_Image.jpg'")
+final_image.save(output_path)
+print(f"Surrealistic image saved as '{output_path}'")
